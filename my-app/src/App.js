9ff4d608d97f24d033849db483590aab7483ce1a
ObiloxYT <AfriaStudios@outlook.in>
2025-08-12 11:21:39 +0000

    omg!

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -79,20 +79,21 @@ function App() {
 
 <p style={{ fontSize: "22px", fontWeight: "500", textAlign: "center" }}>
   The Place For{" "}
-  <RotatingText
-    texts={["Mods", "Texture Packs", "Schematics"]}
-    mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
-    staggerFrom="last"
-    initial={{ y: "100%" }}
-    animate={{ y: 0 }}
-    exit={{ y: "-120%" }}
-    staggerDuration={0.025}
-    splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
-    transition={{ type: "spring", damping: 30, stiffness: 400 }}
-    rotationInterval={2000}
-  />
+  <span style={{ display: "inline-block" }}>
+    <RotatingText
+      texts={["Mods", "Texture Packs", "Schematics"]}
+      mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+      staggerFrom="last"
+      initial={{ y: "100%" }}
+      animate={{ y: 0 }}
+      exit={{ y: "-120%" }}
+      staggerDuration={0.025}
+      splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+      transition={{ type: "spring", damping: 30, stiffness: 400 }}
+      rotationInterval={2000}
+    />
+  </span>
 </p>
-
 
         <div style={{ display: "flex", gap: "15px", flexWrap: "wrap", justifyContent: "center" }}>
           <button style={btnStyle}>Play Now</button>
