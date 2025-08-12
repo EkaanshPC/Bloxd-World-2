e1e6169a9faebb7f5fd1c4b9ecaf9f60c04ee1e4
ObiloxYT <AfriaStudios@outlook.in>
2025-08-12 11:17:26 +0000

    oh my gawd

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import { useRef } from "react";
 import VariableProximity from './VariableProximity';
-
+import RotatingText from './RotatingText'
+  
 function App() {
   const containerRef = useRef(null);
-  const words = ["Bloxd", "World"];
+  const words = ["Bloxd World"];
 
   return (
     <div ref={containerRef} style={{ fontFamily: "sans-serif" }}>
@@ -76,9 +77,22 @@ function App() {
           />
         ))}
 
-        <p style={{ maxWidth: "600px", fontSize: "18px", opacity: 0.8 }}>
-          The Place For BLOXD mods!
-        </p>
+<p style={{ fontSize: "22px", fontWeight: "500", textAlign: "center" }}>
+  The Place For{" "}
+  <RotatingText
+    texts={["Mods", "Texture Packs", "Schematics"]}
+    mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+    staggerFrom="last"
+    initial={{ y: "100%" }}
+    animate={{ y: 0 }}
+    exit={{ y: "-120%" }}
+    staggerDuration={0.025}
+    splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+    transition={{ type: "spring", damping: 30, stiffness: 400 }}
+    rotationInterval={2000}
+  />
+</p>
+
 
         <div style={{ display: "flex", gap: "15px", flexWrap: "wrap", justifyContent: "center" }}>
           <button style={btnStyle}>Play Now</button>
