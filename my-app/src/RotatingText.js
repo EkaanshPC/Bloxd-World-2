7f88898765e1a67093e251657c315685e2762de1
ObiloxYT <AfriaStudios@outlook.in>
2025-08-12 11:51:19 +0000

    augh

--- my-app/src/RotatingText.js
+++ my-app/src/RotatingText.js
@@ -8,7 +8,7 @@ import {
   useMemo,
   useState,
 } from "react";
-import { motion, AnimatePresence } from "motion/react";
+import { motion, AnimatePresence } from "framer-motion";
 
 import "./RotatingText.css";
 
